53f38270d43123a2ce9fd53069d02fad467e7ae4
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-09-28 00:37:46 -0500

    added input field for ip address lookup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import {
+  HashRouter as Router,
+  Route,
+} from 'react-router-dom';
+import IpLookupPage from '../IpLookupPage/IpLookupPage'
 import './App.css';
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <h1>IP Lookup Tool</h1>
-      </div>
+      <Router>
+        <Route
+        exact
+        path="/"
+        component={IpLookupPage}>
+        </Route>
+      </Router>
     );
   }
 }
---
+++ src/components/IpLookupPage/IpLookupPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class IpLookupPage extends Component {
+    state = {
+        ip: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            ip: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            ip: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="form" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="ip address" value={this.state.ip} name="batch"onChange={this.handleChange}/>
+                <button>Submit</button>
+        </form>
+            </div>
+        );
+    }
+
+}
+
+export default IpLookupPage;
