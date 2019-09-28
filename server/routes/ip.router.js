f1d0a40e0b227ce705eac3add1d08bf7fe85c690
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-09-28 12:01:06 -0500

    building out the ip api get route, node is not working for the http request, researching curl requirment

---
+++ server/routes/ip.router.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const router = express.Router();
+const https = require('https');
+
+https.get(`http://ip-api.com/json/{query}`, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const ipRouter = require('./routes/ip.router');
 const port = process.env.PORT || 5000;
 
+/** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json());
 app.use(express.static('build'));
 
+/** ---------- EXPRESS ROUTES ---------- **/
+app.use('/ip', ipRouter);
+
+
+/** ---------- START SERVER ---------- **/
 app.listen(port, function () {
     console.log('Listening on port: ', port);
 });
