42823d3f75ed4cc8d8733849a47fd9a9c7f78413
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-09-28 14:24:53 -0500

    building route to ip api, can render current ip adress

--- server/routes/ip.router.js
+++ server/routes/ip.router.js
@@ -1,21 +1,13 @@
 const express = require ('express');
 const router = express.Router();
-const https = require('https');
 
-https.get(`http://ip-api.com/json/{query}`, (resp) => {
-  let data = '';
+var request = require('request');
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+request('http://ip-api.com/json/{query}', function(err, resp) {
+    if (err) console.log(err.message);
+    else {
+        console.log(resp);
+    }
+})
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
 module.exports = router;
