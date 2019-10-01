517430e6c2299508b4ada60de9c1e87000b0f73e
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-10-01 08:05:50 -0500

    backend edited

--- server/modules/ip-api.js
+++ server/modules/ip-api.js
@@ -1,1 +1,17 @@
-const SCOPES = ['http://ip-api.com/json/'];
+const request = require('request');
+
+function init() {
+  request('http://ip-api.com/json/', function(err, resp) {
+    if (err) console.log(err.message);
+    else {
+        var ipInfo = JSON.parse(resp.body);
+        console.log(resp);
+        console.log('this it var ipInfo', ipInfo);
+    }
+})
+}
+
+
+export {
+  init
+};
